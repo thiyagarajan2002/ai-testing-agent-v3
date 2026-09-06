13cffa86a5777c5f702b82e25824f1db66e57cfb
Thiyagarajan V <64621469+thiyagarajan2002@users.noreply.github.com>
2026-09-07 00:18:56 +0530

    v3.1.0: add test suite model

---
+++ src/main/java/com/thiyagarajan/agent/model/TestSuite.java
@@ -0,0 +1,9 @@
+package com.thiyagarajan.agent.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSuite {
+    public String name = "Test Suite";
+    public List<String> plans = new ArrayList<>();
+}
